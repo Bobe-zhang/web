b91d09ba7eadf2687e8d44eafee44f5546ac0a32
hbo <923682922@qq.com>
2022-07-09 00:22:03 +0800

    xxx

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,21 +2,10 @@
   <div class="about">
     <h1>This is an about page</h1>
     <el-table :data="data" style="width: 100%">
-      <el-table-column prop="prop" label="姓名" width="width"> </el-table-column>
-      <el-table-column prop="prop" label="年龄" width="width"> </el-table-column>
-      <el-table-column prop="prop" label="爱好" width="width"> </el-table-column>
-      <el-table-column prop="prop" label="籍贯" width="width"> </el-table-column>
-      <el-table-column prop="prop" label="籍贯" width="width"> </el-table-column>
-      <el-table-column prop="prop" label="籍贯" width="width"> </el-table-column>
-      <el-table-column prop="prop" label="籍贯" width="width"> </el-table-column>
-      <el-table-column prop="prop" label="籍贯" width="width"> </el-table-column>
-      <el-table-column prop="prop" label="籍贯" width="width"> </el-table-column>
-      <el-table-column prop="prop" label="籍贯" width="width"> </el-table-column>
-    <el-button @click="sunmit">按钮</el-button>
-    <el-table :data="dataList" style="width: 100%">
       <el-table-column prop="prop" label="姓名" width="width"></el-table-column>
       <el-table-column prop="prop" label="年龄" width="width"></el-table-column>
       <el-table-column prop="prop" label="爱好" width="width"></el-table-column>
+      <el-table-column prop="prop" label="籍贯" width="width"></el-table-column>
       <el-table-column prop="prop" label="籍贯" width="width"></el-table-column>
       <el-table-column prop="prop" label="籍贯" width="width"></el-table-column>
       <el-table-column prop="prop" label="籍贯" width="width"></el-table-column>
