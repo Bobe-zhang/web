a025b4c5e0cc3bf6a962f84490dbf06f859ab904
hbo <923682922@qq.com>
2022-07-08 21:41:53 +0800

    ddd

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -13,6 +13,13 @@
       <el-table-column prop="prop" label="籍贯" width="width"> </el-table-column>
       <el-table-column prop="prop" label="籍贯" width="width"> </el-table-column>
       <el-table-column prop="prop" label="籍贯" width="width"> </el-table-column>
+      <el-table-column prop="prop" label="啊哈哈哈" width="width"> </el-table-column>
+      <el-table-column prop="prop" label="啊哈哈哈" width="width"> </el-table-column>
+      <el-table-column prop="prop" label="啊哈哈哈" width="width"> </el-table-column>
+      <el-table-column prop="prop" label="啊哈哈哈" width="width"> </el-table-column>
+      <el-table-column prop="prop" label="啊哈哈哈" width="width"> </el-table-column>
+      <el-table-column prop="prop" label="啊哈哈哈" width="width"> </el-table-column>
+      <el-table-column prop="prop" label="啊哈哈哈" width="width"> </el-table-column>
     </el-table>
   </div>
 </template>
