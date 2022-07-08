7aa1251136582fcb66001a9a8d6b283d25a11626
hbo <923682922@qq.com>
2022-07-08 23:45:12 +0800

    hhh

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <el-button @click="sunmit">按钮</el-button>
     <el-table :data="dataList" style="width: 100%">
       <el-table-column prop="prop" label="姓名" width="width"></el-table-column>
       <el-table-column prop="prop" label="年龄" width="width"></el-table-column>
@@ -11,6 +12,7 @@
       <el-table-column prop="prop" label="籍贯" width="width"></el-table-column>
       <el-table-column prop="prop" label="籍贯" width="width"></el-table-column>
     </el-table>
+    <el-button type="primary">buttonCont</el-button>
   </div>
 </template>
 
@@ -27,20 +29,26 @@ export default {
   watch: {
     dataList: {
       handler(val, oldval) {
-        console.log(val, oldval)
-      }
+        console.log(val, oldval);
+      },
     },
     isture: {
       handler(val) {
-        console.log(val)
-      }
-    }
+        console.log(val);
+      },
+    },
   },
   methods: {
     sunmit() {
+      const a = 123;
+      console.log(a);
       console.log('aaaaa');
-      this.br = !!this.isture;
-    }
-  }
+      if (this.isture) {
+        this.br = true;
+      } else {
+        this.br = false;
+      }
+    },
+  },
 };
 </script>
