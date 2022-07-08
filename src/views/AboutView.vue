02ff85e63090a7d8bf9a24e835cce22e42eb748d
hbo <923682922@qq.com>
2022-07-08 22:18:47 +0800

    xxx

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <el-table :data="data" style="width: 100%">
+    <el-table :data="dataList" style="width: 100%">
       <el-table-column prop="prop" label="姓名" width="width"></el-table-column>
       <el-table-column prop="prop" label="年龄" width="width"></el-table-column>
       <el-table-column prop="prop" label="爱好" width="width"></el-table-column>
@@ -19,12 +19,27 @@ export default {
   name: 'AboutView',
   data() {
     return {
-      data: [],
+      dataList: [],
+      isture: 'ture',
+      br: true,
     };
+  },
+  watch: {
+    dataList: {
+      handler(val, oldval) {
+        console.log(val, oldval)
+      }
+    },
+    isture: {
+      handler(val) {
+        console.log(val)
+      }
+    }
   },
   methods: {
     sunmit() {
-      console.log('aaaaa')
+      console.log('aaaaa');
+      this.br = !!this.isture;
     }
   }
 };
