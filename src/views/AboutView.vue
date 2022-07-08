b007a53809c8266a8a1106b05fa37e96b5682530
hbo <923682922@qq.com>
2022-07-08 21:55:51 +0800

    xxx

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,14 +2,14 @@
   <div class="about">
     <h1>This is an about page</h1>
     <el-table :data="data" style="width: 100%">
-      <el-table-column prop="prop" label="姓名" width="width"> </el-table-column>
-      <el-table-column prop="prop" label="年龄" width="width"> </el-table-column>
-      <el-table-column prop="prop" label="爱好" width="width"> </el-table-column>
-      <el-table-column prop="prop" label="籍贯" width="width"> </el-table-column>
-      <el-table-column prop="prop" label="籍贯" width="width"> </el-table-column>
-      <el-table-column prop="prop" label="籍贯" width="width"> </el-table-column>
-      <el-table-column prop="prop" label="籍贯" width="width"> </el-table-column>
-      <el-table-column prop="prop" label="籍贯" width="width"> </el-table-column>
+      <el-table-column prop="prop" label="姓名" width="width"></el-table-column>
+      <el-table-column prop="prop" label="年龄" width="width"></el-table-column>
+      <el-table-column prop="prop" label="爱好" width="width"></el-table-column>
+      <el-table-column prop="prop" label="籍贯" width="width"></el-table-column>
+      <el-table-column prop="prop" label="籍贯" width="width"></el-table-column>
+      <el-table-column prop="prop" label="籍贯" width="width"></el-table-column>
+      <el-table-column prop="prop" label="籍贯" width="width"></el-table-column>
+      <el-table-column prop="prop" label="籍贯" width="width"></el-table-column>
     </el-table>
   </div>
 </template>
@@ -22,5 +22,10 @@ export default {
       data: [],
     };
   },
+  methods: {
+    sunmit() {
+      console.log('aaaaa')
+    }
+  }
 };
 </script>
